10d29c1658cdbe892ec7175f7a890632dbe4fc53
Danijel Copic <danijelcopic@gmail.com>
2017-10-03 17:04:42 +0200

    Uradjen zadatak izmena vrednosti fixed #8

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,11 +1,20 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Knjiga;
+import model.Oblast;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class Zadatak3IzmenaVrednosti {
+
+	static Dao<Knjiga, Integer> knjigaDao;
+	static Dao <Oblast, Integer> oblastDao;
 
 	public static void main(String[] args) {
 
@@ -15,15 +24,53 @@ public class Zadatak3IzmenaVrednosti {
 
 			connectionSource = new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
 
+			knjigaDao = DaoManager.createDao(connectionSource, Knjiga.class);
+			oblastDao = DaoManager.createDao(connectionSource, Oblast.class);
+
+			TableUtils.clearTable(connectionSource, Knjiga.class);
+			TableUtils.clearTable(connectionSource, Oblast.class);
+
+			Knjiga knjiga1 = new Knjiga("Java programiranje", 650, new Date());
+			knjigaDao.create(knjiga1);
+
+			Knjiga knjiga2 = new Knjiga("Android programiranje", 500, new Date());
+			knjigaDao.create(knjiga2);
+
+
+			Oblast oblast1 = new Oblast("Uvod", 2);
+			oblast1.setKnjiga(knjiga1);
+			oblastDao.create(oblast1);
+
+			Oblast oblast2 = new Oblast("Naredbe", 10);
+			oblast2.setKnjiga(knjiga1);
+			oblastDao.create(oblast2);
+
+			Oblast oblast3 = new Oblast("Aritmeticki operatori", 20);
+			oblast3.setKnjiga(knjiga1);
+			oblastDao.create(oblast3);
+
+			Oblast oblast4 = new Oblast("Android operativni sistem", 2);
+			oblast4.setKnjiga(knjiga2);
+			oblastDao.create(oblast4);
+
+			Oblast oblast5 = new Oblast("Activity klasa", 30);
+			oblast5.setKnjiga(knjiga2);
+			oblastDao.create(oblast5);
 
 
 
+			for(Oblast o : oblastDao.queryForAll()) {
+				System.out.println(o);
+			}
+
+			Oblast oblastZaIzmenu = oblastDao.queryForId(oblast5.getId());
+			oblastZaIzmenu.setPocetnaStrana(35);
+			oblastDao.update(oblastZaIzmenu);
 
 
-
-
-
-
+			for(Oblast o : oblastDao.queryForAll()) {
+				System.out.println(o);
+			}
 
 
 		}catch (Exception e) {
--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,11 +1,17 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Knjiga;
+import model.Oblast;
 
 import java.io.IOException;
 
 public class Zadatak4BrisanjeVrednosti {
+
+	static Dao<Knjiga, Integer> knjigaDao;
+	static Dao <Oblast, Integer> oblastDao;
 
 	public static void main(String[] args) {
 
@@ -15,16 +21,7 @@ public class Zadatak4BrisanjeVrednosti {
 
 			connectionSource = new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
 
-
-
-
-
-
-
-
-
-
-
+			
 
 		}catch (Exception e) {
 			e.printStackTrace();
