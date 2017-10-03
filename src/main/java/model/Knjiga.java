39a02e076fed5d0a23ae3a8327fe8ca402a91ea8
Danijel Copic <danijelcopic@gmail.com>
2017-10-03 12:36:52 +0200

    Uradjena klasa Knjiga fixed #4

---
+++ src/main/java/model/Knjiga.java
@@ -0,0 +1,105 @@
+package model;
+
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+
+@DatabaseTable(tableName = "knjiga")
+public class Knjiga {
+
+	public static final String POLJE_NASLOV = "naslov";
+	public static final String POLJE_BROJ_STRANA = "broj_strana";
+	public static final String POLJE_DATUM_IZDAVANJA = "datum_izdavanja";
+
+	private boolean prisutna;
+
+	@DatabaseField(generatedId = true)
+	private int id;
+
+	@DatabaseField(columnName = POLJE_NASLOV, canBeNull = false)
+	private String naslov;
+
+	@DatabaseField(columnName = POLJE_BROJ_STRANA, canBeNull = false)
+	private int brojStrana;
+
+	@DatabaseField(columnName = POLJE_DATUM_IZDAVANJA, canBeNull = false)
+	private Date datumIzdanja;
+
+	@ForeignCollectionField(foreignFieldName = "knjiga")
+	private ForeignCollection<Oblast> oblasti;
+
+
+
+	public Knjiga() {
+
+	}
+
+
+	public Knjiga(String naslov, int brojStrana, Date datumIzdanja) {
+		this.naslov = naslov;
+		this.brojStrana = brojStrana;
+		this.datumIzdanja = datumIzdanja;
+	}
+
+
+	public boolean isPrisutna() {
+		return prisutna;
+	}
+
+	public void setPrisutna(boolean prisutna) {
+		this.prisutna = prisutna;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNaslov() {
+		return naslov;
+	}
+
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+
+	public int getBrojStrana() {
+		return brojStrana;
+	}
+
+	public void setBrojStrana(int brojStrana) {
+		this.brojStrana = brojStrana;
+	}
+
+	public Date getDatumIzdanja() {
+		return datumIzdanja;
+	}
+
+	public void setDatumIzdanja(Date datumIzdanja) {
+		this.datumIzdanja = datumIzdanja;
+	}
+
+	public ForeignCollection<Oblast> getOblasti() {
+		return oblasti;
+	}
+
+	public void setOblasti(ForeignCollection<Oblast> oblasti) {
+		this.oblasti = oblasti;
+	}
+
+	@Override
+	public String toString() {
+		return "Knjiga{" +
+				"id=" + id +
+				", naslov='" + naslov + '\'' +
+				", brojStrana=" + brojStrana +
+				", datumIzdanja=" + datumIzdanja +
+				'}';
+	}
+}
